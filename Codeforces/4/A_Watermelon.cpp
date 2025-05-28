557c6cf90fb0addcb74eb4690581103d6c9829e5
Md Imam Hasan <info.imamhasan346@gmail.com>
2025-05-27 22:52:27 -0700

    CF Solution

---
+++ Codeforces/4/A_Watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+//Speed
+#define Code_By_Imam ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main()
+{
+    Code_By_Imam
+    int w; cin>>w;
+    if(w==2) cout<<"NO\n";
+    else if(w%2!=0) cout<<"NO\n";
+    else cout<<"YES\n";
+    return 0;
+}
