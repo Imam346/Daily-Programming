8a7c69ea2a85c9bdbbad8dfb970c8b0042f2b102
Md Imam Hasan <info.imamhasan346@gmail.com>
2025-06-12 15:43:05 +0600

    CC Solutions

---
+++ CodeChef/87/A_Tram.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a/gcd(a,b))*b)
+using namespace std;
+
+//pbds
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Speed
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Imam cout.tie(NULL);
+
+int32_t main()
+{
+    Code By Imam
+    int n; cin>>n;
+    int tram=0, passenger=0;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        passenger = passenger - a + b;
+        tram = max(tram, passenger);
+    }
+    cout<<tram<<endl;
+    return 0;
+}
