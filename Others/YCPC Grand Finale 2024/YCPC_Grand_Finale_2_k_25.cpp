857f8589f1b13425986c2c96d70fff4644251eb1
Md Imam Hasan <info.imamhasan346@gmail.com>
2025-08-04 13:43:28 +0600

    YCPC Grand Final 2024

---
+++ Others/YCPC Grand Finale 2024/YCPC_Grand_Finale_2_k_25.cpp
@@ -0,0 +1,13 @@
+/*----------------------------------------
+    author:  Imam
+    created: 01.02.2025 12:26:18
+----------------------------------------*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr); cout.tie(NULL);
+    cout<<"Hello, Coders! Welcome to YCPC Grand Finale 2k25!\n";
+    return 0;
+}
